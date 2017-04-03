a80b238b9596de1e07fe534c2ad23ad3eb50175c
claudiob <claudiob@inventati.org>
2017-04-03 07:33:35 -0700

    Clean up unused code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,6 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
 ]
 SimpleCov.start
 
-Dir['./spec/support/**/*.rb'].each {|f| require f}
-
 RSpec.configure do |config|
   config.order = 'random'
   config.run_all_when_everything_filtered = false
